e28f0f7a9f98ac458e94ec88935e6026dd2da264
kNelakonda <kavi.nelakonda@gmail.com>
2017-10-08 01:35:22 -0700

    limited

--- endpoints/calorie-details.js
+++ endpoints/calorie-details.js
@@ -8,20 +8,18 @@ module.exports = function(req,res,next){
 	var searchTerm = "raw broccoli";
 	var ndbno;
 	request("https://api.nal.usda.gov/ndb/search/?format=json&q="+searchTerm+"&ds=Standard Reference&sort=r&max=25&offset=0&api_key=" + api_key, function (error, response, body) {
-  		//console.log('error:', error); // Print the error if one occurred
- 		//console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
- 		var foodItem = JSON.parse(body).list.item[0];
- 		ndbno = foodItem.ndbno;
- 		console.log("foodItem "+ foodItem);
- 		console.log("ndbno "+ ndbno);
+  	console.log('error:', error); // Print the error if one occurred
+ 	console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+ 	var foodItem = JSON.parse(body).list.item[0];
+ 	ndbno = foodItem.ndbno;
+ 	
 	
 	
 
-	request("https://api.nal.usda.gov/ndb/V2/reports?ndbno="+ndbno+"&type=s&format=json&api_key=" + api_key, function (error, response, body) {
+		request("https://api.nal.usda.gov/ndb/V2/reports?ndbno="+ndbno+"&type=s&format=json&api_key=" + api_key, function (error, response, body) {
   		console.log('error:', error); // Print the error if one occurred
  		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
  		var firstFood = JSON.parse(body).foods[0].food;
- 		//console.log(firstFood);
  		var firstFoodDesc = firstFood["desc"];
  		var firstFoodName = firstFoodDesc.name;
  		var firstFoodNutrients = firstFood.nutrients;
@@ -29,9 +27,10 @@ module.exports = function(req,res,next){
  		result.name = firstFoodName;
  		var nutrients = [];
  		result.nutrients = nutrients;
- 		//console.log(result);
- 		var count = Math.min(firstFoodNutrients.length , 10);
- 		for(var i = 0, parseInt(i) <= parseInt(count), i++){
+ 		var LIMIT = 10
+ 		var count = Math.min(firstFoodNutrients.length , LIMIT);
+ 		var i;
+ 		for( i = 0; i < count; i++){
  			var startNutrient = firstFoodNutrients[i];
  			var name = startNutrient.name;
  			var value = startNutrient.value + " " + startNutrient.unit;
@@ -39,16 +38,9 @@ module.exports = function(req,res,next){
  			nutrient[name.toString()] = value;
  			result.nutrients.push(nutrient);
  		}
- 		// firstFoodNutrients.forEach(function(startNutrient){
- 		// 	var name = startNutrient.name;
- 		// 	var value = startNutrient.value + " " + startNutrient.unit;
- 		// 	var nutrient = {};
- 		// 	nutrient[name.toString()] = value;
- 		// 	result.nutrients.push(nutrient);
- 		// });
  		
  		res.send(result);
  		
-	});
+		});
 	});
 };
